80c805484b59fc0083acdcb97f1e929b2f69439b
Adam L <username@users.noreply.github.com>
2016-12-28 15:31:16 -0800

    Added locations

--- src/elm/Launches/Models.elm
+++ src/elm/Launches/Models.elm
@@ -1,11 +1,27 @@
 module Launches.Models exposing (..)
 
 
+type alias Location =
+    { id : Int
+    , name : String
+    , countryCode : String
+    }
+
+
+initLocation =
+    { id = 0
+    , name = ""
+    , countryCode = ""
+    }
+
+
 type alias Launch =
     { id : Int
     , name : String
     , date : String
     , status : Int
+    , location : Location
+    , description : String
     }
 
 
@@ -15,6 +31,8 @@ initLaunch =
     , name = ""
     , date = ""
     , status = 0
+    , location = initLocation
+    , description = ""
     }
 
 
@@ -33,10 +51,28 @@ tempLaunches =
       , name = "Atlas V 401 | NROL-79"
       , date = "20170120T000000Z"
       , status = 2
+      , location = tempLocation1
+      , description = "Iridium NEXT is a second-generation worldwide network of telecommunications satellites to eventually replace the old Iridium constellation. These satellites will incorporate features such as data transmission which were not emphasized in the original design."
       }
     , { id = 879
       , name = "Falcon 9 Full Thrust | 10 x Iridium NEXT (1-10)"
       , date = "20170105T203600Z"
       , status = 2
+      , location = tempLocation2
+      , description = "his is the first test flight of the Electron rocket. Built and operated by Rocket Lab, Electron is meant to offer low-price launches for the small-sat market."
       }
     ]
+
+
+tempLocation1 =
+    { id = 124
+    , name = "Onenui Station, Mahia Peninsula, New Zealand"
+    , countryCode = "NZL"
+    }
+
+
+tempLocation2 =
+    { id = 40
+    , name = "Vandenberg AFB, CA, USA"
+    , countryCode = "USA"
+    }
--- src/elm/Launches/View.elm
+++ src/elm/Launches/View.elm
@@ -8,14 +8,16 @@ import Launches.Models exposing (Launches, Launch)
 listLaunch : Launch -> Html Msg
 listLaunch launch =
     li []
-        [ text launch.name
+        [ h4 [] [ text launch.name ]
+        , p [] [ text launch.location.name ]
+        , p [] [ text launch.date ]
         ]
 
 
 listView : Launches -> Html Msg
 listView launches =
     div []
-        [ h3 [] [ text "Launchessss" ]
+        [ h3 [] [ text "Upcoming Launches" ]
         , ul []
             (List.map listLaunch launches)
         ]

c41727bee77a6e12b93ede64b3a319973b9d97c7
Adam L <username@users.noreply.github.com>
2016-12-28 15:19:01 -0800

    Mapped launch list

--- src/elm/Launches/View.elm
+++ src/elm/Launches/View.elm
@@ -2,11 +2,20 @@ module Launches.View exposing (..)
 
 import Html exposing (..)
 import Launches.Messages exposing (Msg(..))
-import Launches.Models exposing (Launches)
+import Launches.Models exposing (Launches, Launch)
+
+
+listLaunch : Launch -> Html Msg
+listLaunch launch =
+    li []
+        [ text launch.name
+        ]
 
 
 listView : Launches -> Html Msg
 listView launches =
     div []
-        [ h3 [] [ text "Launches" ]
+        [ h3 [] [ text "Launchessss" ]
+        , ul []
+            (List.map listLaunch launches)
         ]

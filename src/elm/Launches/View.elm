8ee4b656a2a268cacf63045d358cd8054230af95
Adam L <username@users.noreply.github.com>
2017-01-03 00:27:56 -0800

    Added descriptions

--- src/elm/Launches/View.elm
+++ src/elm/Launches/View.elm
@@ -2,40 +2,12 @@ module Launches.View exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
 import Launches.Messages exposing (Msg(..))
 import Launches.Models exposing (Launches, Launch, LaunchId)
 import RemoteData exposing (..)
-import Components.DateHelpers as DateHelpers
 import Components.Copy exposing (copy)
-
-
-itemView : Maybe Launch -> Html Msg
-itemView launch =
-    case launch of
-        Nothing ->
-            div [] [ text copy.launchInactive ]
-
-        Just l ->
-            div []
-                [ h2 [] [ DateHelpers.dateHeading l.isoStart ]
-                , p [] [ text l.location.name ]
-                , h1 [] [ text l.name ]
-                , div [] [ DateHelpers.launchWindow l.wsstamp l.westamp ]
-                , div [] [ DateHelpers.launchTime l.isoStart ]
-                , hr [] []
-                , p [] [ text (toString launch) ]
-                ]
-
-
-listItemView : Launch -> Html Msg
-listItemView launch =
-    li [ onClick (ShowLaunch launch.id) ]
-        [ h4 [] [ text launch.name ]
-        , p [] [ text launch.location.name ]
-        , p [] [ DateHelpers.launchDate launch.isoStart ]
-        , p [] [ text (toString launch.id) ]
-        ]
+import Components.LaunchesList as LaunchesList
+import Components.LaunchesItem as LaunchesItem
 
 
 launchView : Launches -> Maybe Launch -> Html Msg
@@ -51,7 +23,7 @@ launchView model launch =
             div [] [ text copy.launchesError ]
 
         Success data ->
-            itemView launch
+            LaunchesItem.view launch
 
 
 listView : Launches -> List (Html Msg)
@@ -70,7 +42,7 @@ listView model =
             ]
 
         Success data ->
-            (List.map listItemView data)
+            (List.map LaunchesList.view data)
 
 
 view : Launches -> Maybe Launch -> Html Msg
